1f3a82b50b7cede03bb17435c1ec366c0db3924c
Joseph Shearer <joseph@josephshearer.net>
2020-03-31 14:17:02 -0400

    Added reset button, changed mode selector to be more intuitive
    Closes #7

--- src/components/WGS/WGS.tsx
+++ src/components/WGS/WGS.tsx
@@ -1,5 +1,6 @@
 import React from "react";
-import { AppBar, Toolbar, Typography, makeStyles, Theme, Tabs, Tab, Switch, FormControlLabel } from "@material-ui/core";
+import { AppBar, Toolbar, Typography, makeStyles, Theme, Tabs, Tab, IconButton, Icon, Tooltip, ButtonGroup } from "@material-ui/core";
+import { ToggleButtonGroup, ToggleButton } from "@material-ui/lab";
 import { withPieChartData, AnnotationCategory, IPieChartFilter, FilterMode } from "./store";
 import { AspectPie } from "./pie";
 import { useLocation, useHistory, Link } from "react-router-dom";
@@ -7,6 +8,7 @@ import { __RouterContext as RouteContext, useRouteMatch, Redirect } from "react-
 import { Genes } from "./Genes";
 import { Annotations } from "./Annotations";
 import { grey } from "@material-ui/core/colors";
+import RotateLeftIcon from '@material-ui/icons/RotateLeft';
 
 const TabPanel = (props: { children: JSX.Element | JSX.Element[], index: any, value: any }) => {
     const { children, value, index, ...other } = props;
@@ -77,7 +79,7 @@ const operatorHR = {
 
 export const WGS = () => {
     const {
-        P,F,C
+        P, F, C
     } = withPieChartData();
     const styles = withStyles({});
 
@@ -90,8 +92,8 @@ export const WGS = () => {
     const filters: Array<IPieChartFilter> = React.useMemo(
         (
             () => Object.entries(selectedCategories)
-                        .map(([aspect, categories]: [keyof typeof selectedCategories, AnnotationCategory[]]) => categories.map(category => ({aspect, category})))
-                        .reduce((accum,curr) => ([...accum,...curr]), [])
+                .map(([aspect, categories]: [keyof typeof selectedCategories, AnnotationCategory[]]) => categories.map(category => ({ aspect, category })))
+                .reduce((accum, curr) => ([...accum, ...curr]), [])
         ),
         [selectedCategories]
     );
@@ -109,7 +111,7 @@ export const WGS = () => {
 
     const setOperator = (op: FilterMode) => {
         query.set("operator", op);
-        history.push({search: `?${query}`});
+        history.push({ search: `?${query}` });
     }
 
     const match_path = React.useContext(RouteContext).match.path;
@@ -128,33 +130,44 @@ export const WGS = () => {
             <Toolbar />
             <div className={styles.container}>
                 <div className={styles.aboutContainer}>
-                Explore the state of annotation of the Arabidopsis thaliana genome based on annotation status using the Gene Ontology (GO) aspects of Molecular Function, Biological Process, and Cellular Component.
+                    Explore the state of annotation of the Arabidopsis thaliana genome based on annotation status using the Gene Ontology (GO) aspects of Molecular Function, Biological Process, and Cellular Component.
+                </div>
+                <div style={{
+                    width: "100%",
+                    display: "flex",
+                    justifyContent: "flex-end"
+                }}>
+                    <Tooltip title="Reset filters">
+                        <IconButton onClick={() => setSelectedCategories({ F: [], P: [], C: [] })} ><RotateLeftIcon /></IconButton>
+                    </Tooltip>
                 </div>
                 <div className={styles.chartContainer}>
                     <AspectPie data={F} label="Molecular Function" onActiveChange={(actives) => setSelectedCategories(({ ...selectedCategories, F: actives }))} activeCategories={selectedCategories.F} />
                     <AspectPie data={P} label="Biological Process" onActiveChange={(actives) => setSelectedCategories(({ ...selectedCategories, P: actives }))} activeCategories={selectedCategories.P} />
                     <AspectPie data={C} label="Cellular Component" onActiveChange={(actives) => setSelectedCategories(({ ...selectedCategories, C: actives }))} activeCategories={selectedCategories.C} />
                 </div>
-                <Typography variant="h6">Filter: {filters.map(f => `${aspectHR[f.aspect]} - ${statusHR[f.category]}`).join(` ${operatorHR[operator]} `)}</Typography>
-                <FormControlLabel
-                    value="end"
-                    control={<Switch onChange={() => setOperator(operator === 'union' ? 'intersection' : 'union')} checked={operator==='union'} />}
-                    label={operator==='union' ? 'Or' : 'And'}
-                    labelPlacement="end"
-                />
+                <Typography variant="h6">Filter: <ul>{filters.map(f => <span>{aspectHR[f.aspect]} - {statusHR[f.category]}</span>).reduce((accum, curr) => [...accum, <li> {curr} {accum.length < filters.length - 1 ? <b> {operatorHR[operator]} </b> : null} </li>], [])}</ul></Typography>
+                <ToggleButtonGroup
+                        value={operator}
+                        exclusive
+                        onChange={(_, v) => setOperator(v)}
+                    >
+                        <ToggleButton value="union">Or</ToggleButton>
+                        <ToggleButton value="intersection">And</ToggleButton>
+                    </ToggleButtonGroup>
                 <div className={styles.mainContainer}>
                     {
                         sub_match ? (
                             <>
                                 <Tabs value={sub_match.params.route}>
-                                    <Tab component={Link} to={{pathname: "genes", search: ''+query}} value={"genes"} label="Gene IDs" />
-                                    <Tab component={Link} to={{pathname: "annotations", search: ''+query}} value={"annotations"} label="Annotations" />
+                                    <Tab component={Link} to={{ pathname: "genes", search: '' + query }} value={"genes"} label="Gene IDs" />
+                                    <Tab component={Link} to={{ pathname: "annotations", search: '' + query }} value={"annotations"} label="Annotations" />
                                 </Tabs>
                                 <TabPanel value={"annotations"} index={sub_match.params.route}>
-                                    <Annotations filters={filters} operator={operator}/> 
+                                    <Annotations filters={filters} operator={operator} />
                                 </TabPanel>
                                 <TabPanel value={"genes"} index={sub_match.params.route}>
-                                    <Genes filters={filters} operator={operator}/> 
+                                    <Genes filters={filters} operator={operator} />
                                 </TabPanel>
                             </>
                         ) : <Redirect to={`${match_path}/genes`} />
